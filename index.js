312922f6ad06d7b330467f261fdf337cc9e8427d
Nithin1297 <nithin.madduri@proclink.com>
2024-08-06 14:45:36 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
